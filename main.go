42773c706a6c346fc474fc82534189096e000c1c
Zaid Khan <zdkhan@ucdavis.edu>
2019-11-15 11:53:20 -0800

    Update main.go
    change text

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Goodbye World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
